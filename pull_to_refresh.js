9f4926a3f9c53778adafe05268a64b2f14f0f046
GF/2020/4552   Vishal Kumar Sankhyan <71447579+VishalKumarSankhyan@users.noreply.github.com>
2021-08-09 17:02:20 +0530

    Update pull_to_refresh.js

--- pull_to_refresh.js
+++ pull_to_refresh.js
@@ -1,13 +1,12 @@
 mobile_app_body = document.querySelector('.mobile_app_body');
 refresher = document.querySelector('.refresher');
 loading_bar = document.querySelectorAll('.loading_bar');
-
+var other_function_active = false;
 var scroll_active = false;
 var scroll_stop_timer;
 var p_current_y = null;
 var p_start_y = null;
 var changeY = null;
-
 /*-------------------------------------------------- scrollstart --------------------------------------------------*/
 mobile_app_body.addEventListener('scroll', function () {
     scroll_active = true;
@@ -17,28 +16,26 @@ mobile_app_body.addEventListener('scroll', function () {
     clearTimeout(scroll_stop_timer);
     scroll_stop_timer = setTimeout(function () {
         scroll_active = false;
-    }, 300);
+    }, 400);
 })
-
 /*-------------------------------------------------- close refresh --------------------------------------------------*/
-//refresh_close()
-
 function refresh_close() {
     refresher.style.transform = 'translateY(-100px)';
     refresher.style.transitionDuration = 350 + 'ms';
     mobile_app_body.classList.remove('refresh_loading')
     mobile_app_body.style.overflow = '';
+    document.body.style.overflow = '';
 }
 /*-------------------------------------------------- open refresh --------------------------------------------------*/
 function refresh_open() {
     refresher.style.transitionDuration = 350 + 'ms';
-    refresher.style.transform = 'translateY(40px)';
+    refresher.style.transform = 'translateY(45px)';
     mobile_app_body.classList.add('refresh_loading')
-
+    mobile_app_body.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
     for (let i = 0; i < loading_bar.length; i++) {
         loading_bar[i].classList.add('loading_prgrass')
     }
-
     setTimeout(function () {
         for (let i = 0; i < loading_bar.length; i++) {
             loading_bar[i].classList.remove('loading_prgrass')
@@ -49,13 +46,13 @@ function refresh_open() {
 /*-------------------------------------------------- move refresh --------------------------------------------------*/
 function refresh_move(RefreshY) {
     mobile_app_body.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
     refresher.style.transitionDuration = 0 + 'ms';
     refresher.style.transform = "translate3d(" + 0 + "px, " + RefreshY + "px, 0)";
 }
-
 /*-------------------------------------------------- touchstart --------------------------------------------------*/
 mobile_app_body.addEventListener('touchstart', function (event) {
-    if (mobile_app_body.scrollTop === 0 && scroll_active == false) {
+    if (mobile_app_body.scrollTop === 0 && scroll_active == false && other_function_active == false) {
         if (event.type === "touchstart") {
             let touch = event.touches[0];
             p_start_y = touch.clientY;
@@ -64,7 +61,7 @@ mobile_app_body.addEventListener('touchstart', function (event) {
 })
 /*-------------------------------------------------- touchmove --------------------------------------------------*/
 mobile_app_body.addEventListener('touchmove', function (event) {
-    if (mobile_app_body.scrollTop === 0 && scroll_active == false) {
+    if (mobile_app_body.scrollTop === 0 && scroll_active == false && other_function_active == false) {
         if (event.type === "touchmove") {
             let touch = event.touches[0];
             p_current_y = touch.clientY;
@@ -72,6 +69,7 @@ mobile_app_body.addEventListener('touchmove', function (event) {
         changeY = p_start_y < p_current_y ? Math.abs(p_current_y - p_start_y) : 0;
         // move refresh
         if (changeY == 0) { }
+        else if(changeY>(document.body.clientHeight/2)){ }
         else {
             refresh_move(changeY)
         }
@@ -79,9 +77,11 @@ mobile_app_body.addEventListener('touchmove', function (event) {
 })
 /*-------------------------------------------------- touchend --------------------------------------------------*/
 document.body.addEventListener('touchend', function (event) {
-    if (mobile_app_body.scrollTop === 0 && scroll_active == false) {
+    if (mobile_app_body.scrollTop === 0 && scroll_active == false && other_function_active == false) {
         if (event.type === "touchend") {
-            if (changeY == 0) { }
+            if (changeY == 0) { 
+                refresh_close()
+            }
             else {
                 if (changeY > 100) {
                     refresh_open()
